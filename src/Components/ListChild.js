01ac27ce3cfd646f172234389403f1146d2d7049
ipeksargin <ipek.srgn@gmail.com>
2020-09-29 18:13:13 +0300

    listchild close button added

--- src/Components/ListChild.js
+++ src/Components/ListChild.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import PropTypes from 'prop-types';
 
 
 function ListChild(props) {
+  const handleDeleteTask = useCallback(() => {
+    // console.log(props.status);
+    // console.log(props.title);
+    props.closeClick(props.id);
+  }, [props]);
+
   return (
     <div className="row">
       <div className="col">
@@ -11,10 +17,22 @@ function ListChild(props) {
             <h5 className="card-title">{props.title}
               <button
                 type="button"
+                className="close"
+                aria-label="Close"
+                onClick={handleDeleteTask}
+                data-id={props.id}
+                data-title={props.title}
+                data-status={props.status}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+              <button
+                type="button"
                 className="btn btn-outline-dark btn-sm ml-5"
                 onClick={props.click}
                 data-status={props.status}
-              >Move Task</button></h5>
+                data-id={props.id}
+              >Move</button></h5>
             <p className="card-text">{props.description}</p>
           </div>
         </div>
@@ -24,9 +42,11 @@ function ListChild(props) {
 }
 
 ListChild.propTypes = {
+  'id': PropTypes.string.isRequired,
   'title': PropTypes.string.isRequired,
   'description': PropTypes.string.isRequired,
   'click': PropTypes.func.isRequired,
   'status': PropTypes.isRequired,
+  'closeClick': PropTypes.func.isRequired,
 };
 export default ListChild;
